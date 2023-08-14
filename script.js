5b50d5f13c91769c61a32cf1a8375258e6dbe184
Blake Wallace <blakewb.1096@gmail.com>
2023-08-14 11:25:17 -0600

    adds all needed confirms

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var numLength = prompt("How many characters would you like? (8-128)")
+  var numbers = "1234567890"
+  var lwrCaseChar = "abcdefghijklmnopqrstuvwxz"
+  var uprCaseChar = lwrCaseChar.toUpperCase()
+  var specialChar = "1@#$%^&*()"
 
+  var passwordchar = [numbers, lwrCaseChar, uprCaseChar, specialChar] ;
+    
+  
   if (numLength >= 8 && numLength <= 128) {
     console.log(numLength + "")
   }
@@ -12,40 +19,44 @@ function generatePassword() {
     return generatePassword();
   }
 
-  const response = confirm("Would you like to include lowercase letters?")
-  console.log(response);
-  if (response) {
+  const lwrCaseConf = confirm("Would you like to include lowercase letters?")
+  console.log( lwrCaseConf);
+  if ( lwrCaseConf) {
     console.log("OK was pressed");
   }
   else {
     console.log("Cancel was pressed");
   }
 
-  const response2 = confirm("Would you like to include uppercase letters?")
-  if (response2) {
+  const uprCaseConf = confirm("Would you like to include uppercase letters?")
+  if (uprCaseConf) {
     console.log("OK Was pressed");
   }
   else {
     console.log("Cancel was pressed")
   }
 
-  const response3 = confirm("Would you like to include numbers?")
-  if (response3) {
+  const numberConf = confirm("Would you like to include numbers?")
+  if (numberConf) {
     console.log("OK was pressed")
   }
   else {
     console.log("Cancel was pressed")
   }
 
-  const response4 = confirm("Would you like to include special characters?")
-  if (response4) {
+  const specialCharConf = confirm("Would you like to include special characters?")
+  if (specialCharConf) {
     console.log("OK was pressed")
   }
   else {
     console.log("Cancel was pressed")
   }
+
+  return "see this"
+  //function refrence week 3 mini project for mathfloor and math random, take all charecters varible passwordchar .length return varible 
+  // for loop that goes over password lenth and the options and pushes them together ".push <20" push all the otions together then return the result together 
+  
 }
-
 
 
 
